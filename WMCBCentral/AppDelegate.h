e284c5e0c23d0ef4cc6c415663f44645121aea7e
maginawin <maginawin@163.com>
2015-06-03 14:23:18 +0800

    Initial Commit

---
+++ WMCBCentral/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WMCBCentral
+//
+//  Created by maginawin on 15/6/3.
+//  Copyright (c) 2015年 wendong wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WMCBCentral/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WMCBCentral
+//
+//  Created by maginawin on 15/6/3.
+//  Copyright (c) 2015年 wendong wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
